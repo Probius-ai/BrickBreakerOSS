a6fd506bf38d2a8c7f60438ecdf25f95b8fc8ba1
Probius-ai <kcfm0529@naver.com>
2024-05-13 22:12:04 +0900

    Changes to codes that display bricks

--- brickbreaker_flutter/lib/game.dart
+++ brickbreaker_flutter/lib/game.dart
@@ -14,6 +14,8 @@ class BrickBreakerGame  extends FlameGame{
   late   CameraComponent cameraComponent = CameraComponent(world: world)
   ..viewfinder.anchor = Anchor.topLeft;
 
+  List<Brick> bricks = [];
+
   @override
   void onLoad() async{
     // TODO: implement onLoad
@@ -25,7 +27,7 @@ class BrickBreakerGame  extends FlameGame{
 
   @override
   void onMount() {
-    world.add(Brick(gridSize: gridSize)..x =  size.x/2 ..y=size.y/2);
+    world.addAll(bricks);
     addAll([cameraComponent, world]);
   }
 
@@ -45,5 +47,18 @@ class BrickBreakerGame  extends FlameGame{
   Color backgroundColor() {
     // TODO: implement backgroundColor
     return const Color.fromARGB(255, 163, 204, 163);
+  }
+
+  void populateBricks(int rows, int columns){
+    double firstPosX = 0;
+
+
+    // Create Bricks in Game
+    for(int i = 0; i<rows; i++){
+      for(int j =0; j<columns;j++){
+        bricks.add(Brick(gridSize: gridSize)
+        ..position = Vector2(firstPosX + (gridSize.x*j), gridSize.y * i));
+      }
+    }
   }
 }
