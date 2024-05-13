2c71cb27c28da48ea5a6206c23f26cbb93c8d9d7
Probius-ai <kcfm0529@naver.com>
2024-05-13 21:55:36 +0900

    Added Brick

---
+++ brickbreaker_flutter/lib/brick.dart
@@ -0,0 +1,23 @@
+import 'dart:ui';
+
+import 'package:flame/components.dart';
+
+class Brick  extends PositionComponent{
+  late final Vector2 gridSize;
+
+  Brick({required this.gridSize}) : super(size: gridSize);
+  //Configure Brick Color
+  Paint paint = Paint()..color =  Color.fromARGB(255, 88, 68, 100);
+
+  @override
+  void render(Canvas canvas) {
+    // TODO: implement render
+    // Draw Brick to Rectangle
+    canvas.drawRRect(getRRect(), paint);
+  }
+
+  RRect getRRect() {
+    // Configure Rectangle Shape
+    return RRect.fromRectAndRadius(size.toRect(),  Radius.circular(gridSize.x*0.1));
+  }
+}
--- brickbreaker_flutter/lib/game.dart
+++ brickbreaker_flutter/lib/game.dart
@@ -1,5 +1,43 @@
+import 'dart:async';
+import 'dart:ui';
+
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 
 class BrickBreakerGame  extends FlameGame{
-  
+  static double grid = 0.1;
+  static late Vector2 gridSize;
+
+  @override
+  void onLoad() async{
+    // TODO: implement onLoad
+    await Flame.device.setLandscapeLeftOnly();
+    await Future.delayed(const Duration(seconds: 1)); // wait 1s for flame.size to update
+
+    gridSize = Vector2(size.x * grid, size.y *grid); // get phone display size
+  }
+
+  @override
+  void onMount() {
+    // TODO: implement onMount
+    
+  }
+
+  @override
+  void update(double dt) {
+    // TODO: implement update
+    super.update(dt);
+  }
+
+  @override
+  void render(Canvas canvas) {
+    // TODO: implement render
+    super.render(canvas);
+  }
+
+  @override
+  Color backgroundColor() {
+    // TODO: implement backgroundColor
+    return const Color.fromARGB(255, 163, 204, 163);
+  }
 }
